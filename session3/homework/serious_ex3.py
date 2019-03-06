75bc9e0fddff988d979fd53f89a9d9282509aad0
tuandat732 <spt.may7320@gmail.com>
2019-03-06 20:37:08 +0700

    initial commit

--- session3/homework/serious_ex3.py
+++ session3/homework/serious_ex3.py
@@ -1,12 +1,15 @@
 from random import *
 a=['decade','kuuga','ryuki','exaid','ghost','agito','faiz','ghost','blade','kiva','wizard']
 b=a.copy()
+print("fan kamen rider auto win")
 for i in range(len(a)):
     n=list(a[i])
     shuffle(n)
     print(*n)
-    m=input("nhap cau tl(neu chan thi nhap exit): ")
+    m=input("nhập câu tl(nếu chán nhập exit): ")
     if(m=='exit'): break
     elif(a[i]==m):
-        print("hura")
-    else: print("ngu bo")
+        print("chuẩn fan r")
+    else: print("ngu bò")
+        
+        
--- session3/homework/study.py
+++ session3/homework/study.py
@@ -1,6 +1,14 @@
 # #1
-# nested list trong python có thể dc hiểu như là 1 mảng dưới dạng ma trận kích cỡ mxn
+# Danh sách lồng nhau là danh sách xuất hiện dưới dạng một thành phần trong danh sách khác.
+# Để trích xuất một phần tử từ danh sách lồng nhau, chúng ta có thể tiến hành theo hai bước.
+# Đầu tiên, trích xuất danh sách lồng nhau, sau đó trích xuất mục quan tâm.
+# Cũng có thể kết hợp các bước đó bằng cách sử dụng các toán tử khung đánh giá từ trái sang phải.
 b=[[1,2,3],[3,4,5],[5,6,7]]
+#c1
+a=b[2]
+print(a[1])
+#c2
+print(b[2][1])
 for i in range(len(b)):
     print(b[i])
 # #2
